344959e87805fa1c2fa7d558ce41cc49e663ea62
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-15 11:18:30 +0300

    format files

--- lua/nik/plugins/move.lua
+++ lua/nik/plugins/move.lua
@@ -1,21 +1,21 @@
 return {
-    "fedepujol/move.nvim",
-    config = function()
-        local opts = {
-            noremap = true,
-            silent = true,
-        }
-        require("move").setup({})
-        -- Normal-mode commands
-        vim.keymap.set("n", "<A-Up>", ":MoveLine(-1)<CR>", opts)
-        vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>", opts)
-        vim.keymap.set("n", "<A-Left>", ":MoveWord(-1)<CR>", opts)
-        vim.keymap.set("n", "<A-Right>", ":MoveWord(1)<CR>", opts)
+  "fedepujol/move.nvim",
+  config = function()
+    local opts = {
+      noremap = true,
+      silent = true,
+    }
+    require("move").setup({})
+    -- Normal-mode commands
+    vim.keymap.set("n", "<A-Up>", ":MoveLine(-1)<CR>", opts)
+    vim.keymap.set("n", "<A-Down>", ":MoveLine(1)<CR>", opts)
+    vim.keymap.set("n", "<A-Left>", ":MoveWord(-1)<CR>", opts)
+    vim.keymap.set("n", "<A-Right>", ":MoveWord(1)<CR>", opts)
 
-        -- Visual-mode commands
-        vim.keymap.set("v", "<A-Up>", ":MoveBlock(-1)<CR>", opts)
-        vim.keymap.set("v", "<A-Down>", ":MoveBlock(1)<CR>", opts)
-        vim.keymap.set("v", "<A-Left>", ":MoveHBlock(-1)<CR>", opts)
-        vim.keymap.set("v", "<A-Right>", ":MoveHBlock(1)<CR>", opts)
-    end,
+    -- Visual-mode commands
+    vim.keymap.set("v", "<A-Up>", ":MoveBlock(-1)<CR>", opts)
+    vim.keymap.set("v", "<A-Down>", ":MoveBlock(1)<CR>", opts)
+    vim.keymap.set("v", "<A-Left>", ":MoveHBlock(-1)<CR>", opts)
+    vim.keymap.set("v", "<A-Right>", ":MoveHBlock(1)<CR>", opts)
+  end,
 }
