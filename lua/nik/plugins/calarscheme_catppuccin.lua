d54d6e68c79c39c8363dc6f90a1c14864f1d6411
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-15 11:35:17 +0300

    add another catppucchin them

--- lua/nik/plugins/calarscheme_catppuccin.lua
+++ lua/nik/plugins/calarscheme_catppuccin.lua
@@ -3,6 +3,9 @@ return {
   priority = 1000,
   name = "catppuccin",
   config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
     vim.cmd.colorscheme("catppuccin")
   end,
 }
