c6302354384564d84d30991fa3b00f3df6d601a5
«Nick-voz» <nik.vozisow@gmail.com>
2025-03-26 18:36:30 +0300

    feat: Add ruff to mason and cleanup mason config
    - Add "ruff" to mason-tool-installer ensure_installed list
    - Remove comments in mason config for better readability

--- lua/nik/plugins/lsp/mason.lua
+++ lua/nik/plugins/lsp/mason.lua
@@ -6,15 +6,12 @@ return {
   },
 
   config = function()
-    -- import mason
     local mason = require("mason")
 
-    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
 
     local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -38,13 +35,14 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
         "eslint_d",
         "debugpy",
         "pylint",
+        "ruff",
       },
     })
   end,
