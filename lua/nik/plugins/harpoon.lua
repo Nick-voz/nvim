2b1e9763edd2efbd7ae821647ecf5d3e6f4b7b95
Nick-voz <nik.vozisow@gmail.com>
2025-10-18 17:15:21 +0300

    feat: Add harpoon plugin configuration
    This commit introduces the configuration for the `ThePrimeagen/harpoon` plugin.
    
    - Add `lua/nik/plugins/harpoon.lua` for harpoon setup.
    - Configure harpoon plugin with `harpoon2` branch and `nvim-lua/plenary.nvim` dependency.
    - Map `<leader>ha` to add the current file to the harpoon list.
    - Map `<leader>hf`, `<leader>hs`, `<leader>ht` to jump to the 1st, 2nd, and 3rd files respectively in the harpoon list.
    - Map `<leader>hp` to go to the previous file in the harpoon list.
    - Map `<leader>hn` to go to the next file in the harpoon list.
    - Implement a custom Telescope picker for harpoon files.
    - Map `<leader>hl` to open the Telescope picker for the harpoon list.

---
+++ lua/nik/plugins/harpoon.lua
@@ -0,0 +1,60 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set("n", "<leader>ha", function()
+      harpoon:list():add()
+    end, { desc = "Add file to the harpoon list" })
+    -- vim.keymap.set("n", "<leader>hl", function()
+    --   harpoon.ui:toggle_quick_menu(harpoon:list())
+    -- end, { desc = "Toggle harpoon list" })
+
+    vim.keymap.set("n", "<leader>hf", function()
+      harpoon:list():select(1)
+    end, { desc = "Goto first file in the harpoon list" })
+    vim.keymap.set("n", "<leader>hs", function()
+      harpoon:list():select(2)
+    end, { desc = "Goto second file in the harpoon list" })
+    vim.keymap.set("n", "<leader>ht", function()
+      harpoon:list():select(3)
+    end, { desc = "Goto third file in the harpoon list" })
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<leader>hp", function()
+      harpoon:list():prev()
+    end, { desc = "Goto previous file in the harpoon list" })
+    vim.keymap.set("n", "<leader>hn", function()
+      harpoon:list():next()
+    end, { desc = "Goto next file in the harpoon list" })
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    vim.keymap.set("n", "<leader>hl", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
+  end,
+}
