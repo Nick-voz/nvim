a675a342b1784a69a0d834b972c09f83bb77b35c
Nick-voz <nik.vozisow@gmail.com>
2025-10-28 19:15:10 +0300

    feat: Make floating windows transparent in Catppuccin
    - Configure `catppuccin` plugin to enable transparency for floating windows.
    - Add `float = { transparent = true }` to the `catppuccin.setup` options.

--- lua/nik/plugins/calarscheme_catppuccin.lua
+++ lua/nik/plugins/calarscheme_catppuccin.lua
@@ -6,6 +6,9 @@ return {
     require("catppuccin").setup({
       flavour = "mocha",
       transparent_background = true,
+      float = {
+        transparent = true,
+      },
     })
     vim.cmd.colorscheme("catppuccin")
   end,
