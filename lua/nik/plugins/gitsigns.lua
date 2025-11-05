235d0232813889ae838382ff9e8261027342b767
Nick-voz <nik.vozisow@gmail.com>
2025-11-05 18:32:42 +0300

    style: Remove unnecessary comments and blank lines
    Remove comments that were purely organizational and did not add functional value.
    Remove blank lines to improve code compactness and readability.

--- lua/nik/plugins/gitsigns.lua
+++ lua/nik/plugins/gitsigns.lua
@@ -4,16 +4,13 @@ return {
   opts = {
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
-
       local function map(mode, l, r, desc)
         vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
       end
 
-      -- Navigation
       map("n", "]h", gs.next_hunk, "Next Hunk")
       map("n", "[h", gs.prev_hunk, "Prev Hunk")
 
-      -- Actions
       map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
       map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
       map("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
