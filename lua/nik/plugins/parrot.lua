2f2f3b8069d1d22854ad7154b5a1773fb933555a
Nick-voz <nik.vozisow@gmail.com>
2026-03-18 14:31:36 +0300

    feat(parrot): add terminal mode support to commit message keymap
    - Extend keymap modes from { "n", "v" } to { "n", "v", "t" } for <leader>pcm
    - Rename key from <leader>pm to <leader>pcm for consistency

--- lua/nik/plugins/parrot.lua
+++ lua/nik/plugins/parrot.lua
@@ -171,7 +171,12 @@ return {
     vim.keymap.set("v", "<leader>pd", ":PrtWriteDocks<CR>", { desc = "Generate documentation for selected symbol" })
     vim.keymap.set("v", "<leader>pr", ":PrtReview<CR>", { desc = "Review selection" })
 
-    vim.keymap.set({ "n", "v" }, "<leader>pm", "<cmd>PrtCommitMsg<cr>", { desc = "Create commit message with AI" })
+    vim.keymap.set(
+      { "n", "v", "t" },
+      "<leader>pcm",
+      "<cmd>PrtCommitMsg<cr>",
+      { desc = "Create commit message with AI" }
+    )
   end,
   vim.keymap.set("v", "<leader>pt", ":PrtUnitTests<CR>", { desc = "write unit tests" }),
 }
