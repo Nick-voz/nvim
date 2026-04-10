fa1832a21d285d456ab3fae89d6dcf5f38ed1bf8
Nick-voz <nik.vozisow@gmail.com>
2026-04-10 15:42:24 +0300

    refactor(plugins): replace treesitter-modules with native nvim-treesitter config
    - Remove treesitter-modules.lua plugin configuration
    - Add install configuration to treesitter.lua with languages: json, yaml, html, css, markdown, bash, lua, vim, dockerfile, gitignore, python, javascript, fish
    - Drop angular and markdown_inline from ensure_installed list

--- lua/nik/plugins/treesitter.lua
+++ lua/nik/plugins/treesitter.lua
@@ -2,4 +2,21 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").install({
+      "json",
+      "yaml",
+      "html",
+      "css",
+      "markdown",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
+      "python",
+      "javascript",
+      "fish",
+    })
+  end,
 }
