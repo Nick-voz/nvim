fe3825c5deabb13f29e5ce40c484d65f1861df4e
Nick-voz <nik.vozisow@gmail.com>
2026-04-23 07:56:03 +0300

    refactor(snacs): simplify configuration and keymaps
    - Remove dim and dashboard from opts
    - Remove quickfile from opts
    - Remove dim keymaps (<leader>df, <leader>dd)
    - Remove debug init autocommand with global functions

--- lua/nik/plugins/snacs.lua
+++ lua/nik/plugins/snacs.lua
@@ -4,12 +4,9 @@ return {
   lazy = false,
 
   opts = {
-    dim = { enabled = true },
-    dashboard = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true, prompt_pos = "title" },
     picker = { enabled = true },
-    quickfile = { enabled = true },
     notifier = { enabled = true, timeout = 4000 },
   },
   keys = {
@@ -20,41 +17,5 @@ return {
       end,
       desc = "Notification History",
     },
-    {
-      "<leader>df",
-      function()
-        Snacks.dim.enable()
-      end,
-      desc = "Dim focus",
-    },
-    {
-      "<leader>dd",
-      function()
-        Snacks.dim.disable()
-      end,
-      desc = "Disable dim",
-    },
   },
-
-  init = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-        _G.dd = function(...)
-          Snacks.debug.inspect(...)
-        end
-        _G.bt = function()
-          Snacks.debug.backtrace()
-        end
-
-        if vim.fn.has("nvim-0.11") == 1 then
-          vim._print = function(_, ...)
-            dd(...)
-          end
-        else
-          vim.print = _G.dd
-        end
-      end,
-    })
-  end,
 }
