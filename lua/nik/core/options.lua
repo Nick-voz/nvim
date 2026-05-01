fdc19a537595c91d4d121a05f4c70d6ca5dfcaca
Nick-voz <nik.vozisow@gmail.com>
2026-05-01 10:44:44 +0300

    feat(options): enable split window below as default
    - Add `opt.splitbelow = true` to configure default split direction

--- lua/nik/core/options.lua
+++ lua/nik/core/options.lua
@@ -23,6 +23,7 @@ opt.signcolumn = "yes"
 opt.backspace = "indent,eol,start"
 opt.clipboard:append("unnamedplus")
 opt.splitright = true
+opt.splitbelow = true
 opt.swapfile = false
 
 opt.scrolloff = 999
