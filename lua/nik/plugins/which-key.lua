a1ce003d478b556dde409d22e343ea031da03bb9
Nick-voz <nik.vozisow@gmail.com>
2026-05-01 14:46:02 +0300

    feat(which-key): add configuration options
    - Enable helix preset for which-key
    - Configure window settings:

--- lua/nik/plugins/which-key.lua
+++ lua/nik/plugins/which-key.lua
@@ -1,6 +1,17 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+
+  opts = {
+    preset = "helix",
+    win = {
+      no_overlap = true,
+      width = 45,
+      height = { min = 4, max = 25 },
+      row = 0,
+    },
+  },
+
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
