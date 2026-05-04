b44bed65941feb01caf860388969771062e30414
Nick-voz <nik.vozisow@gmail.com>
2026-05-04 09:39:25 +0300

    feat(nik/core): add automatic directory change from CLI argument
    - Check if single CLI argument is provided using vim.fn.argc()
    - Retrieve argument path with vim.fn.argv(0)
    - Change current directory with nvim_set_current_dir() if argument is a directory

--- lua/nik/core/init.lua
+++ lua/nik/core/init.lua
@@ -1,3 +1,8 @@
+if vim.fn.argc() == 1 then
+  local arg = vim.fn.argv(0)
+  if vim.fn.isdirectory(arg) == 1 then vim.api.nvim_set_current_dir(arg) end
+end
+
 require("nik.core.options")
 require("nik.core.keymaps")
 require("nik.core.autocmd")
